a7cad2bcaa96560364eb7fb53fe06837cfa28934
Walker Hutchinson <57651882+Walkerrh@users.noreply.github.com>
2024-05-05 20:49:19 -0400

    Create polygon.js

---
+++ U2/polygon.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+  beginShape();
+  vertex(100,50);
+  vertex(400,100);
+  vertex(450,300);
+  vertex(200,450);
+  vertex(50,350);
+  endShape(CLOSE);
+}
+
+function keyPressed()
+{
+  save("polygon.png")
+}
